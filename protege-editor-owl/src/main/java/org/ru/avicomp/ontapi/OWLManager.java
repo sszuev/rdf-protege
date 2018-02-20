a80e227abcadc16b34a42846469e52bf2fee576a
szuev <sss.zuev@gmail.com>
2018-02-20 12:56:15 +0300

    ont-protege: cosmetic changes in manaregs factory

--- protege-editor-owl/src/main/java/org/ru/avicomp/ontapi/OWLManager.java
+++ protege-editor-owl/src/main/java/org/ru/avicomp/ontapi/OWLManager.java
@@ -2,33 +2,53 @@ package org.ru.avicomp.ontapi;
 
 import org.semanticweb.owlapi.manchestersyntax.parser.ManchesterOWLSyntaxParserImpl;
 import org.semanticweb.owlapi.model.OWLDataFactory;
-import org.semanticweb.owlapi.model.OWLOntologyManager;
 import org.semanticweb.owlapi.model.OntologyConfigurator;
 import org.semanticweb.owlapi.util.mansyntax.ManchesterOWLSyntaxParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.avicomp.ontapi.OntManagers;
+import ru.avicomp.ontapi.OntologyManager;
 
 /**
  * Created by @szuev on 13.02.2018.
  */
 public class OWLManager {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OWLManager.class);
+
     public static ManchesterOWLSyntaxParser createManchesterParser() {
         return createManchesterParser(new OntologyConfigurator(), getOWLDataFactory());
     }
 
     public static ManchesterOWLSyntaxParser createManchesterParser(OntologyConfigurator conf, OWLDataFactory factory) {
         // todo: from owlapi-parsers, must be removed
-        return new ManchesterOWLSyntaxParserImpl(conf, factory);
+        ManchesterOWLSyntaxParser res = new ManchesterOWLSyntaxParserImpl(conf, factory);
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("ManchesterOWLSyntaxParser: {}", res);
+        }
+        return res;
     }
 
-    public static OWLOntologyManager createOWLOntologyManager() {
-        return OntManagers.createONT();
+    public static OntologyManager createOWLOntologyManager() {
+        OntologyManager res = OntManagers.createONT();
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("OWLOntologyManager: {}", res);
+        }
+        return res;
     }
 
-    public static OWLOntologyManager createConcurrentOWLOntologyManager() {
-        return OntManagers.createConcurrentONT();
+    public static OntologyManager createConcurrentOWLOntologyManager() {
+        OntologyManager res = OntManagers.createConcurrentONT();
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("OWLOntologyManager: {}", res);
+        }
+        return res;
     }
 
     public static OWLDataFactory getOWLDataFactory() {
-        return OntManagers.getDataFactory();
+        OWLDataFactory res = OntManagers.getDataFactory();
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("OWLDataFactory: {}", res);
+        }
+        return res;
     }
 }
