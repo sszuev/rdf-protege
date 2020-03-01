8ccd2cdb6c4a718456988f8cc4c1d6cff797aa74
szuev <sss.zuev@gmail.com>
2020-03-01 18:15:04 +0300

    rdf-protege: minor in OWLUtils - replace ONT-API specific optimization with the standard OWLOntology#containsAnnotationPropertyInSignature(IRI)

--- protege-editor-owl/src/main/java/org/protege/editor/owl/model/util/OWLUtilities.java
+++ protege-editor-owl/src/main/java/org/protege/editor/owl/model/util/OWLUtilities.java
@@ -1,51 +1,36 @@
 package org.protege.editor.owl.model.util;
 
+import org.github.owlcs.ontapi.OWLManager;
 import org.protege.editor.owl.model.OWLModelManager;
 import org.semanticweb.owlapi.model.*;
 
 import java.util.Collection;
 import java.util.Set;
 
-public class OWLUtilities {
+public final class OWLUtilities {
+	private static final OWLAnnotationProperty OWL_DEPRECATED = OWLManager.getOWLDataFactory().getOWLDeprecated();
 
-	private OWLUtilities() {
-	}
-	
-    public static boolean isDeprecated(OWLModelManager manager, OWLObject o) {
-    	if (!(o instanceof OWLEntity)) {
-    		return false;
-    	}
+	public static boolean isDeprecated(OWLModelManager manager, OWLObject o) {
+		if (!(o instanceof OWLEntity)) {
+			return false;
+		}
 		Set<OWLOntology> activeOntologies = manager.getActiveOntologies();
 		return isDeprecated((OWLEntity) o, activeOntologies);
-    }
+	}
 
-	private static boolean isDeprecated(OWLEntity o, Collection<OWLOntology> ontologies) {
+	private static boolean isDeprecated(OWLEntity entity, Collection<OWLOntology> ontologies) {
 		return ontologies.stream()
-				.filter(OWLUtilities::process) // for speedup - a temporary solution (todo: need to fix ONT=API)
-				.flatMap(x -> x.annotationAssertionAxioms(o.getIRI()))
+				.filter(OWLUtilities::hasDeprecated)
+				.flatMap(o -> o.annotationAssertionAxioms(entity.getIRI()))
 				.filter(a -> a.getProperty().isDeprecated())
 				.map(OWLAnnotationAssertionAxiom::getValue)
-				.filter(v -> v instanceof OWLLiteral)
+				.filter(OWLAnnotationValue::isLiteral)
 				.map(v -> (OWLLiteral) v)
 				.filter(OWLLiteral::isBoolean)
 				.anyMatch(OWLLiteral::parseBoolean);
 	}
 
-	private static boolean process(OWLOntology o) {
-		Boolean res = hasDeprecated(o);
-		return res == null || res;
-	}
-
-	private static boolean hasDeprecated(com.github.owlcs.ontapi.Ontology ont) {
-		return ont.asGraphModel().getBaseGraph()
-				.contains(org.apache.jena.graph.Node.ANY,
-						com.github.owlcs.ontapi.jena.vocabulary.OWL.deprecated.asNode(), org.apache.jena.graph.Node.ANY);
-	}
-
-	private static Boolean hasDeprecated(OWLOntology ont) {
-		if (!(ont instanceof com.github.owlcs.ontapi.Ontology)) {
-			return null;
-		}
-		return hasDeprecated((com.github.owlcs.ontapi.Ontology) ont);
+	public static boolean hasDeprecated(OWLOntology ont) {
+		return ont.containsAnnotationPropertyInSignature(OWL_DEPRECATED.getIRI());
 	}
 }
